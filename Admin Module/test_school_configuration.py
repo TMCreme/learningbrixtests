a0c0269160f5b7385e1cbe81d7d8a06665b99f81
TMCreme <tonnybright123@yahoo.com>
2026-05-22 19:38:37 +0000

    fix: cleanup code for a new architecture

--- Admin Module/test_school_configuration.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import expect
-
-
-def test_school_configuration(page):
-    page.get_by_role("button", name="Configuration").click()
-    page.get_by_role("textbox", name="Enter school address").click()
-    page.get_by_role("textbox", name="Enter school address").fill("Tamal")
-    page.get_by_role("button", name="Update").click()
-    expect(page.get_by_text("School Profile Successfully")).to_be_visible()
